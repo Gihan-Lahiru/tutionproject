fd6367da2da6bdff48d81fc5604fd9c16ed8d7c0
Gihan Lahiru <gihanbimsara2001@gmail.com>
2026-05-14 14:33:34 +0530

    Add server listen for local development
    Added server startup for local development.

--- backend/server.js
+++ backend/server.js
@@ -91,5 +91,7 @@ app.use((req, res) => {
 })
 
 // Server startup removed for serverless deployment (Vercel)
-
+app.listen(PORT, () => {
+    console.log(`🚀 Server running on http://localhost:${PORT}`)
+})
 module.exports = app
